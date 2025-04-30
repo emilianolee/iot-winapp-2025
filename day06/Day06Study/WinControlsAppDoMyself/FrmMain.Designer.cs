024e7464dae212911f3264f3fb998fd513ec8151
emilianolee <yechan5172@daum.net>
2025-04-30 17:26:45 +0900

    포트폴리오 작업 -ing 중간 커밋

--- day06/Day06Study/WinControlsAppDoMyself/FrmMain.Designer.cs
+++ day06/Day06Study/WinControlsAppDoMyself/FrmMain.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
             TopPanel = new Panel();
             PicX = new PictureBox();
@@ -35,10 +36,18 @@
             ListBoxSongs = new ListBox();
             BtnSelectSongs = new Button();
             WindowsMediaApp = new AxWMPLib.AxWindowsMediaPlayer();
-            label1 = new Label();
+            LblDeveloper = new Label();
+            BtnClearSongs = new Button();
+            BtnClearAllSongs = new Button();
+            LblTitle = new Label();
+            LblCurrTime = new Label();
+            LblTotalTime = new Label();
+            TrbProgress = new TrackBar();
+            timer1 = new System.Windows.Forms.Timer(components);
             TopPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)PicX).BeginInit();
             ((System.ComponentModel.ISupportInitialize)WindowsMediaApp).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)TrbProgress).BeginInit();
             SuspendLayout();
             // 
             // TopPanel
@@ -49,13 +58,13 @@
             TopPanel.Dock = DockStyle.Top;
             TopPanel.Location = new Point(0, 0);
             TopPanel.Name = "TopPanel";
-            TopPanel.Size = new Size(680, 38);
+            TopPanel.Size = new Size(527, 38);
             TopPanel.TabIndex = 0;
             // 
             // PicX
             // 
             PicX.Image = (Image)resources.GetObject("PicX.Image");
-            PicX.Location = new Point(644, 0);
+            PicX.Location = new Point(489, 0);
             PicX.Name = "PicX";
             PicX.Size = new Size(36, 35);
             PicX.SizeMode = PictureBoxSizeMode.Zoom;
@@ -78,51 +87,129 @@
             ListBoxSongs.Font = new Font("맑은 고딕", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
             ListBoxSongs.FormattingEnabled = true;
             ListBoxSongs.ItemHeight = 17;
-            ListBoxSongs.Location = new Point(460, 44);
+            ListBoxSongs.Location = new Point(12, 44);
             ListBoxSongs.Name = "ListBoxSongs";
-            ListBoxSongs.Size = new Size(208, 259);
+            ListBoxSongs.Size = new Size(300, 191);
             ListBoxSongs.TabIndex = 1;
             ListBoxSongs.SelectedIndexChanged += ListBoxSongs_SelectedIndexChanged;
             // 
             // BtnSelectSongs
             // 
-            BtnSelectSongs.BackColor = Color.OrangeRed;
+            BtnSelectSongs.BackColor = Color.Red;
             BtnSelectSongs.FlatStyle = FlatStyle.Flat;
+            BtnSelectSongs.Font = new Font("Maiandra GD", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             BtnSelectSongs.ForeColor = Color.White;
-            BtnSelectSongs.Location = new Point(460, 309);
+            BtnSelectSongs.Location = new Point(332, 44);
             BtnSelectSongs.Name = "BtnSelectSongs";
-            BtnSelectSongs.Size = new Size(208, 44);
+            BtnSelectSongs.Size = new Size(120, 30);
             BtnSelectSongs.TabIndex = 2;
-            BtnSelectSongs.Text = "Select Songs";
+            BtnSelectSongs.Text = "Select";
             BtnSelectSongs.UseVisualStyleBackColor = false;
             BtnSelectSongs.Click += BtnSelectSongs_Click;
             // 
             // WindowsMediaApp
             // 
             WindowsMediaApp.Enabled = true;
-            WindowsMediaApp.Location = new Point(12, 44);
+            WindowsMediaApp.Location = new Point(1000, 1000);
             WindowsMediaApp.Name = "WindowsMediaApp";
             WindowsMediaApp.OcxState = (AxHost.State)resources.GetObject("WindowsMediaApp.OcxState");
             WindowsMediaApp.Size = new Size(442, 309);
             WindowsMediaApp.TabIndex = 3;
+            WindowsMediaApp.Visible = false;
             // 
-            // label1
+            // LblDeveloper
             // 
-            label1.AutoSize = true;
-            label1.ForeColor = SystemColors.ControlDarkDark;
-            label1.Location = new Point(282, 357);
-            label1.Name = "label1";
-            label1.Size = new Size(138, 15);
-            label1.TabIndex = 4;
-            label1.Text = "Developed By : Emiliano";
+            LblDeveloper.AutoSize = true;
+            LblDeveloper.ForeColor = SystemColors.ControlDarkDark;
+            LblDeveloper.Location = new Point(377, 315);
+            LblDeveloper.Name = "LblDeveloper";
+            LblDeveloper.Size = new Size(138, 15);
+            LblDeveloper.TabIndex = 4;
+            LblDeveloper.Text = "Developed By : Emiliano";
+            // 
+            // BtnClearSongs
+            // 
+            BtnClearSongs.BackColor = Color.Blue;
+            BtnClearSongs.FlatStyle = FlatStyle.Flat;
+            BtnClearSongs.Font = new Font("Maiandra GD", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BtnClearSongs.ForeColor = Color.White;
+            BtnClearSongs.Location = new Point(332, 84);
+            BtnClearSongs.Name = "BtnClearSongs";
+            BtnClearSongs.Size = new Size(120, 30);
+            BtnClearSongs.TabIndex = 2;
+            BtnClearSongs.Text = "Clear";
+            BtnClearSongs.UseVisualStyleBackColor = false;
+            BtnClearSongs.Click += BtnClearSongs_Click;
+            // 
+            // BtnClearAllSongs
+            // 
+            BtnClearAllSongs.BackColor = Color.Indigo;
+            BtnClearAllSongs.FlatStyle = FlatStyle.Flat;
+            BtnClearAllSongs.Font = new Font("Maiandra GD", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BtnClearAllSongs.ForeColor = Color.White;
+            BtnClearAllSongs.Location = new Point(332, 124);
+            BtnClearAllSongs.Name = "BtnClearAllSongs";
+            BtnClearAllSongs.Size = new Size(120, 30);
+            BtnClearAllSongs.TabIndex = 2;
+            BtnClearAllSongs.Text = "Clear All";
+            BtnClearAllSongs.UseVisualStyleBackColor = false;
+            BtnClearAllSongs.Click += BtnClearAllSongs_Click;
+            // 
+            // LblTitle
+            // 
+            LblTitle.AutoSize = true;
+            LblTitle.Location = new Point(161, 312);
+            LblTitle.Name = "LblTitle";
+            LblTitle.Size = new Size(47, 15);
+            LblTitle.TabIndex = 5;
+            LblTitle.Text = "곡 제목";
+            // 
+            // LblCurrTime
+            // 
+            LblCurrTime.AutoSize = true;
+            LblCurrTime.Location = new Point(12, 264);
+            LblCurrTime.Name = "LblCurrTime";
+            LblCurrTime.Size = new Size(87, 15);
+            LblCurrTime.TabIndex = 6;
+            LblCurrTime.Text = "현재 재생 시간";
+            // 
+            // LblTotalTime
+            // 
+            LblTotalTime.AutoSize = true;
+            LblTotalTime.Location = new Point(428, 264);
+            LblTotalTime.Name = "LblTotalTime";
+            LblTotalTime.Size = new Size(87, 15);
+            LblTotalTime.TabIndex = 7;
+            LblTotalTime.Text = "전체 재생 시간";
+            // 
+            // TrbProgress
+            // 
+            TrbProgress.Cursor = Cursors.Hand;
+            TrbProgress.Location = new Point(98, 264);
+            TrbProgress.Maximum = 100;
+            TrbProgress.Name = "TrbProgress";
+            TrbProgress.Size = new Size(328, 45);
+            TrbProgress.TabIndex = 8;
+            TrbProgress.TickFrequency = 0;
+            TrbProgress.TickStyle = TickStyle.None;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
             // 
             // FrmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(680, 380);
-            Controls.Add(label1);
+            ClientSize = new Size(527, 339);
+            Controls.Add(TrbProgress);
+            Controls.Add(LblTotalTime);
+            Controls.Add(LblCurrTime);
+            Controls.Add(LblTitle);
+            Controls.Add(LblDeveloper);
             Controls.Add(WindowsMediaApp);
+            Controls.Add(BtnClearSongs);
+            Controls.Add(BtnClearAllSongs);
             Controls.Add(BtnSelectSongs);
             Controls.Add(ListBoxSongs);
             Controls.Add(TopPanel);
@@ -134,6 +221,7 @@
             TopPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)PicX).EndInit();
             ((System.ComponentModel.ISupportInitialize)WindowsMediaApp).EndInit();
+            ((System.ComponentModel.ISupportInitialize)TrbProgress).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -146,6 +234,13 @@
         private ListBox ListBoxSongs;
         private Button BtnSelectSongs;
         private AxWMPLib.AxWindowsMediaPlayer WindowsMediaApp;
-        private Label label1;
+        private Label LblDeveloper;
+        private Button BtnClearSongs;
+        private Button BtnClearAllSongs;
+        private Label LblTitle;
+        private Label LblCurrTime;
+        private Label LblTotalTime;
+        private TrackBar TrbProgress;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- day06/Day06Study/WinControlsAppDoMyself/FrmMain.cs
+++ day06/Day06Study/WinControlsAppDoMyself/FrmMain.cs
@@ -7,24 +7,56 @@ namespace WinControlsAppDoMyself
         public FrmMain()
         {
             InitializeComponent();
+
+            WindowsMediaApp.Size = new Size(1, 1);
+
+            timer1.Interval = 1000;
+            timer1.Start();
         }
 
-        // create globla variables of String Type Array to save the title or name of the Tracks and path of the track
-        string[] paths, files;
+        List<string> paths = new List<string>();
+        List<string> files = new List<string>();
+
         private void BtnSelectSongs_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Multiselect = true,     // 여러 곡 선택 허용
+                Filter = "Audio Files (*.mp3;*.wav)|*.mp3;*.wav|MP3 Files (*.mp3)|*.mp3|WAV Files (*.wav)|*.wav|All Files (*.*)|*.*"
+            };
 
             if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                files = ofd.SafeFileNames;
-                paths = ofd.FileNames;
-
-                // Display the music titles in listbox
-                for (int i = 0; i < files.Length; i++)
+                // 새로운 파일들을 추가로 누적
+                for (int i = 0; i < ofd.FileNames.Length; i++)
                 {
-                    ListBoxSongs.Items.Add(files[i]);   // Display songs in ListBox
+                    string fileName = ofd.SafeFileNames[i];
+                    string filePath = ofd.FileNames[i];
+
+                    if (!files.Contains(fileName))
+                    {
+                        files.Add(fileName);
+                        paths.Add(filePath);
+                        ListBoxSongs.Items.Add(fileName);
+                    }
+                    else
+                    {
+                        DialogResult result = MessageBox.Show("중복된 곡을 선택하셨습니다. 그래도 추가하시겠습니까?", "알림",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                        if (result == DialogResult.Yes)
+                        {
+                            files.Add(fileName);
+                            paths.Add(filePath);
+                            ListBoxSongs.Items.Add(fileName);
+                        }
+                    }
                 }
+            }
+            else
+            {
+                MessageBox.Show("추가할 곡을 선택하세요!", "오류",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -39,13 +71,21 @@ namespace WinControlsAppDoMyself
         {
             try
             {
-                if (paths != null && ListBoxSongs.SelectedIndex >= 0 && ListBoxSongs.SelectedIndex<paths.Length)
+                if (paths != null && ListBoxSongs.SelectedIndex >= 0 && ListBoxSongs.SelectedIndex < paths.Count)
                 {
                     // 현재 재생 중인 곡 정지
                     WindowsMediaApp.Ctlcontrols.stop();
 
-                    // 선택된 곡 재생
+                    // 약간의 지연을 주어 완전히 정지하도록 함
+                    System.Threading.Thread.Sleep(100);     // 100ms 기다림
+
+                    // 곡의 URL 설정
                     WindowsMediaApp.URL = paths[ListBoxSongs.SelectedIndex];
+
+                    // 반드시 위치를 처음으로 초기화
+                    WindowsMediaApp.Ctlcontrols.currentPosition = 0;
+
+                    // 선택된 곡 재생
                     WindowsMediaApp.Ctlcontrols.play();
                 }
             }
@@ -53,6 +93,69 @@ namespace WinControlsAppDoMyself
             {
                 MessageBox.Show($"노래를 재생하는 중 오류가 발생했습니다 : {ex.Message}", "오류",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnClearSongs_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = ListBoxSongs.SelectedIndex;
+
+            if (selectedIndex >= 0)
+            {
+                // 리스트에서 삭제
+                ListBoxSongs.Items.RemoveAt(selectedIndex);
+
+                // files와 paths에서도 삭제
+                var tempFiles = files.ToList();
+                var tempPaths = paths.ToList();
+                tempFiles.RemoveAt(selectedIndex);
+                tempPaths.RemoveAt(selectedIndex);
+                files = tempFiles;
+                paths = tempPaths;
+            }
+            else
+            {
+                MessageBox.Show("삭제할 곡을 선택하세요", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnClearAllSongs_Click(object sender, EventArgs e)
+        {
+            if (ListBoxSongs.Items.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("모든 곡을  삭제하시겠습니까?", "확인",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    ListBoxSongs.Items.Clear();
+                    files.Clear();
+                    paths.Clear();
+
+                    WindowsMediaApp.Ctlcontrols.stop();
+                }
+            }
+            else
+            {
+                MessageBox.Show("삭제할 곡이 없습니다", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (WindowsMediaApp.currentMedia != null)
+            {
+                LblCurrTime.Text = WindowsMediaApp.Ctlcontrols.currentPositionString;
+                LblTotalTime.Text = WindowsMediaApp.currentMedia.durationString;
+
+                double current = WindowsMediaApp.Ctlcontrols.currentPosition;
+                double total = WindowsMediaApp.currentMedia.duration;
+
+                if (total > 0)
+                {
+                    int percentage = (int)((current / total) * 100);
+                    TrbProgress.Value = Math.Min(percentage, 100);
+                }
             }
         }
     }
